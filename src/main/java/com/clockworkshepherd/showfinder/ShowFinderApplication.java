7215a4f900a26e7820f5ad1cb0412d27ae0cc239
Warwick1156 <warwick1156@gmail.com>
2022-04-07 08:33:14 +0200

    SpringBoot application

---
+++ src/main/java/com/clockworkshepherd/showfinder/ShowFinderApplication.java
@@ -0,0 +1,13 @@
+package com.clockworkshepherd.showfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowFinderApplication.class, args);
+	}
+
+}
